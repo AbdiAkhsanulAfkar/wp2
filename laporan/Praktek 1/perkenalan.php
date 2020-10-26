8303e8fcba44fd7f5dac2626eb62bc195cd666e9
jundix <akhsanulafkar@gmail.com>
2020-10-26 18:54:19 +0700

    needs for middle exam

---
+++ laporan/Praktek 1/perkenalan.php
@@ -0,0 +1,12 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index(){
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya abdi
+                Saya tinggal di suatu desa
+                tidak ada
+                olahraga yang saya sukai 
+                ";
+    }
+}
+?>
---
+++ pustaka-booking/application/controllers/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index(){
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya abdi akhsanul afkar
+                Saya tinggal di suatu desa
+                olahraga yang saya sukai adalah
+                tidak ada";
+    }
+}
+?>
